e48082a8e29b3d27886585b158395dfe828b7427
Roshan Nepal <roshannepalastro@gmail.com>
2022-08-17 19:44:32 +0545

    Added A text view in chat activity

--- app/src/main/java/com/example/myapplication/chat/ChatActivity.kt
+++ app/src/main/java/com/example/myapplication/chat/ChatActivity.kt
@@ -3,11 +3,20 @@ package com.example.myapplication.chat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
-    }
 
+        super.onCreate(savedInstanceState)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val textView = binding.TVText
+        val text = "This Feature is getting Integrated very soon"
+        textView.text = text
+        textView.textSize = 24.0F
+    }
 }
