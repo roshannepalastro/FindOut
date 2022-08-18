e9c035fca6c8359a178ae048436bc809539300e8
Roshan Nepal <roshannepalastro@gmail.com>
2022-08-18 10:04:30 +0545

    Adding new functionality in Profile

---
+++ app/src/main/java/com/example/myapplication/activities/user_profile/ProfileCardHostFragment.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.activities.user_profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+
+
+class ProfileCardHostFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_card_host, container, false)
+    }
+
+
+}
