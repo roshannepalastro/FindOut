06b60d3fb301e993135aac655404b70bb4e1709a
DELL <roshannepalastro@gmail.com>
2022-08-18 00:20:23 -0700

    Trying out new pc

--- app/src/main/java/com/example/myapplication/activities/user_profile/ProfileCardHostFragment.kt
+++ app/src/main/java/com/example/myapplication/activities/user_profile/ProfileCardHostFragment.kt
@@ -1,22 +1,36 @@
 package com.example.myapplication.activities.user_profile
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentProfileCardHostBinding
+import com.example.myapplication.databinding.FragmentProfileCollapseBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
 
 
 class ProfileCardHostFragment : Fragment() {
 
+    private var _binding: FragmentProfileCardHostBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_card_host, container, false)
+    ): View {
+        _binding = FragmentProfileCardHostBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // val adapter = ProfileNewsfeedAdapter()
     }
 
 
